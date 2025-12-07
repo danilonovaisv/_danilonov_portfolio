7151070d9335115c86ec44d56f14eb6d9b3bc48b
danilonovaisv <danilo_novais@yahoo.com.br>
2025-12-07 07:55:26 -0300

    build: Regenerate Next.js output and update Firebase hosting assets.

---
+++ .next/server/app/portfolio.segments/_tree.segment.rsc
@@ -0,0 +1,2 @@
+:HL["/_next/static/chunks/cc03eba6ea6b57fa.css","style"]
+0:{"buildId":"IcaLc_uaJErUQov8NFyGA","tree":{"name":"","paramType":null,"paramKey":"","hasRuntimePrefetch":false,"slots":{"children":{"name":"portfolio","paramType":null,"paramKey":"portfolio","hasRuntimePrefetch":false,"slots":{"children":{"name":"__PAGE__","paramType":null,"paramKey":"__PAGE__","hasRuntimePrefetch":false,"slots":null,"isRootLayout":false}},"isRootLayout":false}},"isRootLayout":true},"staleTime":300}
---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,21 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/2e957baccb55bd08.js",
+    "static/chunks/c5104c0a2b9df4e8.js",
+    "static/chunks/6740f161f60c6ab5.js",
+    "static/chunks/c645af7d6b65f73e.js",
+    "static/chunks/turbopack-2a911bddc22b70a8.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
